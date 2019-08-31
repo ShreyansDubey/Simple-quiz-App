5e083cb2b01d8eb04ecf399cb3d71c480b22b1cf
ShreyansDubey <theshreyansdubey@gmail.com>
2019-08-31 16:26:46 +0530

    quiz answers checked on backend

---
+++ quiz/static/quiz_answers_stringify.js
@@ -0,0 +1,45 @@
+//Makes a string out of the options chosen by the user
+
+//gets a list of all questions
+
+document.getElementById('submit_quiz').onclick = function() {
+
+    q_elements = document.getElementsByClassName('q');
+    ques = []
+    for (let i = 0; i < q_elements.length; i++) {
+        ques[i] = q_elements[i].innerText; 
+    }
+
+    answers_selected = []
+    for (let i = 0; i < ques.length; i++) {
+        try{
+            curr_ans = document.querySelector('input[name="' + ques[i] + '"]:checked').value;
+        }
+        catch(err) {
+            curr_ans = null
+        }
+        if(curr_ans == null) {
+            answers_selected[i] = 'Z';
+        } 
+        else {
+            answers_selected[i] = curr_ans;
+        }
+    }
+    //Now answers selected has a list of all the answers
+    //console.log(answers_selected);
+    ans_selected_string = "";
+    for (let i = 0; i < answers_selected.length; i++) {
+        if(i != answers_selected.length - 1)
+            ans_selected_string += (answers_selected[i] + ' ');   
+        else
+            ans_selected_string += (answers_selected[i]);   
+    }
+    //Setting the hidden form field value
+    document.getElementById('ans').value = ans_selected_string;
+
+    //Submitting the form with answers
+    document.getElementById('ans_form').submit();
+}
+
+
+
--- quiz/takeQuiz/views.py
+++ quiz/takeQuiz/views.py
@@ -9,9 +9,35 @@ def quiz_list(request) :
     return render(request, 'quiz_list.html', {'q_names_ids' : zip(quiz_names, quiz_ids)})
 
 def attempt_quiz(request) :
+    all_quizzes = quizzes.objects.all()
+    if request.method == 'POST' :
+        q_ans = str(request.POST.get('quiz_ans_string', ''))    
+        q_idx = int(request.POST.get('q_idx', ''))
+        quiz_req = None
+        for q in all_quizzes :
+            if int(q_idx) == q.quiz_index :
+                quiz_req = q.quiz_string
+                break
+        #list to contain answers
+        ans = []
+        quiz_questions = quiz_req.split('|')
+        for i in range(len(quiz_questions)) :
+            quiz_questions[i] = quiz_questions[i].split(',')
+            ans.append(quiz_questions[i][-1])
+        q_ans = q_ans.split(' ')
+        # print(q_ans)
+        # print(ans)
+        correct = 0
+        for i in range(len(q_ans)) :
+            if q_ans[i] == ans[i] :
+                correct += 1            
+        # print(correct)
+        # print(len(ans) - correct)
+
+
+
     #getting the quiz index from the link clicked
     quiz_idx = request.GET.get('quiz_idx')
-    all_quizzes = quizzes.objects.all()
     quiz_req = None
     for q in all_quizzes :
         if int(quiz_idx) == q.quiz_index :
@@ -32,14 +58,15 @@ def attempt_quiz(request) :
     quiz_number = []
     for idx in range(len(quiz_questions)) :
         quiz_number.append(idx+1)
-        quiz_q.append(quiz_questions[i][0])
-        quiz_op_A.append(quiz_questions[i][1])
-        quiz_op_B.append(quiz_questions[i][2])
-        quiz_op_C.append(quiz_questions[i][3])
+        quiz_q.append(quiz_questions[idx][0])
+        quiz_op_A.append(quiz_questions[idx][1])
+        quiz_op_B.append(quiz_questions[idx][2])
+        quiz_op_C.append(quiz_questions[idx][3])
 
     #context_dict
     d = {
-        'q_questions' : zip(quiz_number, quiz_q, quiz_op_A, quiz_op_B, quiz_op_C)
+        'q_questions' : zip(quiz_number, quiz_q, quiz_op_A, quiz_op_B, quiz_op_C),
+        'q_idx' : quiz_idx,
     }
     return render(request, 'attempt_quiz.html', d)
     
